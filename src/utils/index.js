ba519b57f189be7adb76b2b0c8a8840ac2ff6a79
Jamil Safdar <jamil.safdar@ao.com>
2021-11-03 11:17:12 +0000

    added yargs

---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const addMovie = (movieObj) => {
+    try {
+        const stringyObj = JSON.stringify(movieObj);
+        fs.writeFileSync('./storage.json', stringyObj);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const listMovies = () => {
+    try {
+        const jsonList = fs.readFileSync('./storage.json');
+    const list = JSON.parse(jsonList);
+    console.log(list);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    addMovie,
+    listMovies
+}
